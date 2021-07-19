60f5b0ce4961a64582493647ea3a7181e54955b5
regaliv <regaliv@example.com>
2021-07-20 01:27:05 +0300

    Прогресс3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello system");
+        System.out.println("");
+        System.out.println("Hello system");
+        System.out.println("Hello system");
+        System.out.println("Hello system");
+        System.out.println("");
         System.out.println("Hello system");
         System.out.println("Hello system");
         System.out.println("Hello system");
         System.out.println("Hello system");
-        System.out.println("Hello system");
-        System.out.println("Hello system");
-        System.out.println("Hello system");
-        System.out.println("Hello system");
-        System.out.println("Hello system");
-        System.out.println("Hello system");
+        System.out.println("");
     }
 }
